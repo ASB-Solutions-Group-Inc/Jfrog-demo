3adb7cbcce246c5fac1d6bd61ddc70e8eef41555
abambah <abambah@google.com>
2022-05-13 17:56:48 -0500

    Adding k8 secret for jfrog docker image pull

--- bootstrap/gke-cluster-init.sh
+++ bootstrap/gke-cluster-init.sh
@@ -5,6 +5,11 @@ if [[ -z "${PROJECT_ID}" ]]; then
   echo "The value of PROJECT_ID is not set. Be sure to run export PROJECT_ID=YOUR-PROJECT first"
   return
 fi
+if [[-z "${JFROG_API_KEY}"]]; then
+  echo "Provide jfrog API key"
+  return
+fi
+
 # Test cluster
 echo "creating testcluster..."
 gcloud beta container --project "$PROJECT_ID" clusters create-auto "testcluster" \
@@ -24,3 +29,12 @@ gcloud beta container --project "$PROJECT_ID" clusters create-auto "prodcluster"
 --subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
 --cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async
 echo "Creating clusters! Check the UI for progress"
+
+gcloud container clusters get-credentials testcluster --project=$PROJECT_ID --zone=$ZONE
+kubectl create secret docker-registry regcred --docker-server=$JFROG_SERVER_NAME --docker-username=$JFROG_USER --docker-password=$JFROG_API_KEY
+# Stage
+gcloud container clusters get-credentials stagingcluster --project=$PROJECT_ID --zone=$ZONE
+kubectl create secret docker-registry regcred --docker-server=$JFROG_SERVER_NAME --docker-username=$JFROG_USER --docker-password=$JFROG_API_KEY
+#prod
+gcloud container clusters get-credentials prodcluster --project=$PROJECT_ID --zone=$ZONE
+kubectl create secret docker-registry regcred --docker-server=$JFROG_SERVER_NAME --docker-username=$JFROG_USER --docker-password=$JFROG_API_KEY
